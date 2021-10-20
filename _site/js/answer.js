eb0ba90a9060a7b04650bb119449d5de9b6469dd
18t2003g <18t2003g@shinshu-u.ac.jp>
2021-10-20 12:24:12 +0900

    答え表示機能の実装

---
+++ _site/js/answer.js
@@ -0,0 +1,13 @@
+/*
+
+show answer
+2021/10/20 by Kenta Asahi
+MIT lic.
+
+*/
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('ansbtn').addEventListener("click", function(){
+        document.getElementById('answer').style.display = "block";
+    });
+});
