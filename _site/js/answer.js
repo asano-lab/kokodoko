3be40edb81049f3a99d5363e69f97faa14885467
18t2003g <18t2003g@shinshu-u.ac.jp>
2021-10-20 16:28:58 +0900

    ヒントno

--- _site/js/answer.js
+++ _site/js/answer.js
@@ -7,7 +7,16 @@ MIT lic.
 */
 
 document.addEventListener("DOMContentLoaded", function(){
+    //answer (legacy)
     document.getElementById('ansbtn').addEventListener("click", function(){
         document.getElementById('answer').style.display = "block";
     });
+
+    //hint(s)
+    const hints = document.getElementsByClassName('hint');
+    for(let i = 0; i < hints.length; i++){
+        hints[i].addEventListener("click", function(){
+            this.parentElement.getElementsByTagName("li")[0].style.display = "block";
+        });
+    }
 });
