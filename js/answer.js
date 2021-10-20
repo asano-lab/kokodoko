a26601581a0fb471962ea37d14313e168e295fd5
Kenta Asahi <18t2003g@shinshu-u.ac.jp>
2021-10-20 17:41:58 +0900

    soanswer.jsに使用例を追加

--- js/answer.js
+++ js/answer.js
@@ -1,27 +1,51 @@
-/*
+/*=========================
 
-show answer
+show answer & hints
 2021/10/20 by Kenta Asahi
 MIT lic.
 
-*/
+==========================*/
 
+//DOM onload
 document.addEventListener("DOMContentLoaded", function(){
-    //answer (legacy)
-    const ans = document.getElementById('ansbtn');
-    if(ans != null){
-        ans.addEventListener("click", function(){
+
+    //answer
+    /*
+    =====Markdown CODE SAMPLE=====
+
+    [答えを表示する](javascript:void(0)){: #ansbtn}
+    >XX県YY市　ZZ市役所
+    {: #answer}
+    */
+    const ansbtn = document.getElementById('ansbtn');
+    if(ansbtn != null){
+        ansbtn.addEventListener("click", function(){
             document.getElementById('answer').style.display = "block";
         });
     }
+
     
-    //answer
-    document.getElementById('ans').addEventListener("click", function(){
-        this.parentElement.getElementsByTagName("li")[0].style.display = "block";
-    });
-
-
+    //answer(new way)
+    /*
+    =====Markdown CODE SAMPLE=====
+    
+    - [答えを表示する](javascript:void(0)){: #ans}  
+       - XX県YY市　ZZ市役所  ##### line including answer MUST BE indented!
+    */
+    const ans = document.getElementById('ans');
+    if(ans != null){
+        ans.addEventListener("click", function(){
+            this.parentElement.getElementsByTagName("li")[0].style.display = "block";
+        });
+    }
+    
     //hint(s)
+    /*
+    =====Markdown CODE SAMPLE=====
+    
+    - [ヒント](javascript:void(0)){: .hint}  
+       - OO県です
+    */
     const hints = document.getElementsByClassName('hint');
     for(let i = 0; i < hints.length; i++){
         hints[i].addEventListener("click", function(){
